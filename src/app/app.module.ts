29ff82b8def17bbc22898330c7d171ebd145fddb
Guillaume Girou <porude-git@yahoo.fr>
2016-12-10 13:30:53 +0100

    step-07-solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 // MATERIAL DESIGN MODULES
 import { MaterialModule } from '@angular/material';
 
-// import { APP_ROUTES } from './app.routes';
+import { APP_ROUTES } from './app.routes';
 
 import { PeopleAppComponent } from './app.component';
 import { HomeComponent } from './home/index';
@@ -15,8 +16,9 @@ import { PersonComponent } from './person/index';
   imports: [
     BrowserModule,
     HttpModule,
+    RouterModule,
     MaterialModule.forRoot(),
-    // APP_ROUTES
+    APP_ROUTES
   ],
   declarations: [
     PeopleAppComponent,
